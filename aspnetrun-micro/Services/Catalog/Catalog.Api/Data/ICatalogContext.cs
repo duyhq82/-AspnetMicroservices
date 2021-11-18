efe7ba0b6bb0d16b16fd09beabc9d6c9327b1a5f
mailman <quocduyho.mail@gmail.com>
2021-11-18 16:39:09 +0700

    Catalog

---
+++ aspnetrun-micro/Services/Catalog/Catalog.Api/Data/ICatalogContext.cs
@@ -0,0 +1,10 @@
+﻿using Catalog.Api.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
